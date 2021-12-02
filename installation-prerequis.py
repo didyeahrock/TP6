db0abc7473a94f6c8d7bb1df9d5d9db31be914d7
didyeahrock <didyeahrock@gmail.com>
2021-12-02 17:53:48 +0100

    Versions de GLPI en variables

--- installation-prerequis.py
+++ installation-prerequis.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
-"""
-    installation-prerequis.py
+"""   installation-prerequis.py
     Script to install a GLPI server with a maria DB database and apache2 server
     Author :    Didier Lemaitre
     Version :   0.1
@@ -30,10 +29,11 @@ import os
         mysql_secure_installation
 
 """
-
 #constants Définitions
 
 LOG_FILE = "./installation.log"
+GLPI_VER = "9.5.6"
+
 
 # Log File Configuration 
 
@@ -52,38 +52,26 @@ try:
     os.system("apt update ; apt upgrade")
     os.system("dpkg --configure -a")
     os.system("apt install -y apache2 php mariadb-server curl python3 python3-pip libmariadbclient-dev pip php-mysql php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu php7.4-intl php7.4-bz2 php7.4-zip")
- #   os.system("sudo apt install -y apache2 php mariadb-server curl python3 python3-pip libmariadbclient-dev pip")
- #   os.system("sudo apt install -y php-mysql php-json php-gd php-curl php-mbstring php-cas php-xml php-cli php-imap php-ldap php-xmlrpc php-apcu")
- #   os.system("sudo apt-get install -y php7.4-intl")
- #   os.system("sudo apt-get install -y php7.4-bz2")
- #   os.system("sudo apt-get install -y php7.4-zip")
     os.system("pip install upgrade setuptools")
     os.system("pip install validators")
     os.system("pip install mariadb")
     os.system("mysql_secure_installation")
 # Installation of GLPI
     os.system("cd /tmp")
-    os.system("wget https://github.com/glpi-project/glpi/releases/download/9.5.6/glpi-9.5.6.tgz")
-    os.system("tar -zxvf glpi-9.5.6.tgz ")
+    wget = "wget https://github.com/glpi-project/glpi/releases/download/{}/glpi-{}.tgz".format(GLPI_VER,GLPI_VER)
+#    os.system("wget https://github.com/glpi-project/glpi/releases/download/9.5.6/glpi-9.5.6.tgz")
+    os.system(wget)
+    tar_glpi = "tar -zxvf glpi-{}.tgz".format(GLPI_VER)
+    os.system(tar_glpi)
+#    os.system("tar -zxvf glpi-9.5.6.tgz ")
 # Moving /tmp/glpi to the root folder of apache2 
     os.system("mv glpi /usr/share/")
 # make www-data owner of GLPI folder
     os.system("chown -R www-data /usr/share/glpi")
-# setting apache2 for GLPI
-#    os.system("sudo cp -v /etc/apache2/apache2.conf /etc/apache2/apache2.conf.default")
-#    os.system("sudo cp -v apache2.conf /etc/apache2/")
-#    os.system("sudo cp -v glpi.net.conf /etc/apache2/sites-available/")
-#   os.system("sudo cp hosts /etc/")
-# Apache reload
-#    os.system("sudo systemctl reload apache2")
-# activation of the GLPI configuration
-#    os.system("sudo a2ensite glpi.net.conf")
-# reload apache
-#    os.system("sudo systemctl reload apache2")
     logging.info("system update and paquet installations succeeded !")
     exit(0)
 except Exception as e:
-    print("paquets installation failed !")
+    print("p.5.6aquets installation failed !")
     logging.error("paquets installation failed !")
     logging.error(e)
     raise e
